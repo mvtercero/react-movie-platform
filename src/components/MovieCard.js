3f0fbfb2baedc6feac1f3f685e90ed09ed564c6a
María Vedia <mvtercero85@gmail.com>
2019-10-19 18:20:24 +0200

    feat(Movie List): create fetch and extract id info

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 
-const MovieCard = ({keyId, poster, title, year }) => {
+const MovieCard = ({keyId, poster, title, year, movieID }) => {
+  const movieIdURL = `http://www.omdbapi.com/?${movieID}&apikey=b55524ae`
   return (
   <li id={keyId}>
     <img src={poster} alt="imagen de la película"/>
     <h2>{title}</h2>
     <p>{year}</p>
+    <p>{movieID}</p>
   </li>
   ) 
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ export default class MovieList extends Component {
  constructor(props) {
    super(props)
    this.state = {
-      movies: []
+    movies: [],
    }
  }
 
@@ -16,24 +16,42 @@ export default class MovieList extends Component {
     })
     .then((movies) => {
       this.setState({ movies: movies.Search })
+      const movieIDList = movies.Search.map(movie => movie.imdbID)
+      for (const movieID of movieIDList) {
+        fetch('http://www.omdbapi.com/?i=' + movieID + '&apikey=b55524ae')
+        .then(function(response) {
+            return response.json();
+        })
+        .then((movieDetail)=> {
+          console.log(movieDetail, "---respuesta de la api")
+        })
+        .catch(function(error) {
+          console.log('Request failed', error)
+        })
+      }
     })
     .catch((error)=>{ console.log(error)});
  }
  
   render() {
-    const { movies } = this.state;  
+    const { movies } = this.state;
     if(movies) {
       return (
         <div>
-          <ul>{movies.map((movie, index) => 
-            <MovieCard 
-              key={index}
-              keyId={index} 
-              poster={movie.Poster} 
-              title={movie.Title} 
-              year={movie.Year}>
-            </MovieCard>)}
+          <ul>
+            { movies.map((movie, index) =>
+              <MovieCard 
+                key={index}
+                keyId={index} 
+                poster={movie.Poster} 
+                title={movie.Title} 
+                year={movie.Year}
+                movieID={movie.imdbID}
+              >
+              </MovieCard>
+            )}
           </ul>
+          <ul></ul>
         </div>
       )
     } else {

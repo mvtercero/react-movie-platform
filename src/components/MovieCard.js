fef74901d626d37305dbe5481bd76e35b084db0d
María Vedia <mvtercero85@gmail.com>
2019-10-20 12:47:53 +0200

    feat(Movie Card): implement a button info

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react'
 import MovieDetail from './MovieDetail'
 
 export default class MovieCard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+     displayDetail: false,
+    }
+  }
+
+  displayDetail = () => {
+    this.setState({
+        displayDetail: !this.state.displayDetail
+    })
+  }
+  
   render() {
     const {keyId, poster, title, year, movieDetail} = this.props
+    const {displayDetail} = this.state
     return (
       <div>
         <li id={keyId}>
           <img src={poster} alt="imagen de la película"/>
           <h2>{title}</h2>
           <p>{year}</p>
+          <button onClick={this.displayDetail}>Más info</button>
         </li>
 
         <ul> 
         { movieDetail.map((detail, index) =>
-          keyId === index ? (
+         displayDetail && keyId === index ? (
           <MovieDetail
             key={index}
             keyId={index}
